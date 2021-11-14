3c9bb34a08ebcd69fb36c54789c4313340c75d13
Etho <58352929+EthoIRL@users.noreply.github.com>
2021-11-13 23:02:41 -0800

    Placeholder Todos

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using Server.Utils;
 
 namespace Server
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        public void ProcessRequest()
+        {
+            // RsaUtils.RsaDecrypt(bytes, RsaCryptoService.ExportParameters(true), false)
+            // Handle RSA Handshake (EG: Give (Server Public key -> Client), Get (Client -> Client Public Key))
+            
+            //Check if account data exists blah blah blah...
+
+            byte[] fileAssemblyBytes = File.ReadAllBytes("a");
+
+            var obfuscatedAssembly = AssemblyUtils.GetObfuscatedAssembly(fileAssemblyBytes);
+
+            // Use Client's RSA Public Key & Encrypt to send over network.
+            // var encryptedAssembly = RsaUtils.RsaEncrypt(obfuscatedAssembly, rsaCryptoService.ExportParameters(false), false);
         }
     }
 }
---
+++ Server/Utils/AssemblyUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Server.Utils
+{
+    public static class AssemblyUtils
+    {
+        public static byte[] GetObfuscatedAssembly(byte[] assembly)
+        {
+            
+        }
+    }
+}
