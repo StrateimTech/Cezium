4382a15776ccf0ffd36380ba0b623be27d94d41b
Etho <58352929+EthoIRL@users.noreply.github.com>
2022-02-27 12:41:56 -0800

    Complete placeholder

--- Cezium/Web/Front/Assets/Js/Mouse/Mouse.js
+++ Cezium/Web/Front/Assets/Js/Mouse/Mouse.js
@@ -15,40 +15,6 @@
         InvertMouseY.disabled = true;
         InvertMouseWheel.disabled = true;
     }
-
-    var person = {"FirstName":"Andrew","LastName":"Lock","Age":"31"};
-    $.ajax({
-        type: "POST",
-        url: "/Mouse?handler=FindUser",
-        // data: {id: id},
-        contentType: "application/json; charset=utf-8",
-        // dataType: "json",
-        // AntiforgeryToken is required by RazorPages
-        headers: {
-            RequestVerificationToken: $('input:hidden[name="__RequestVerificationToken"]').val()
-        },
-        data: JSON.stringify(person)
-    }).done(function () {
-        alert("success");
-    }).fail(function () {
-        alert("error");
-    });
-
-
-    // $.ajax({
-    //     type: 'POST',
-    //     // Note the difference in url (this works if you're actually in Index page)
-    //     url: '/mouse?handler=FindUser',
-    //     headers: {
-    //         RequestVerificationToken: $('input:hidden[name="__RequestVerificationToken"]').val()
-    //     },
-    //     success: function (data) {
-    //         alert(data);
-    //     },
-    //     error: function (error) {
-    //         alert("Error: " + error);
-    //     }
-    // })
 }
 
 function handleDebugState(e) {
---
+++ Cezium/Web/Front/Assets/Js/Rust/Rust.js
@@ -0,0 +1,46 @@
+﻿function handleRustState(e) {
+    const content = document.getElementById("content");
+
+    if (content.style.opacity === "0.4") {
+        content.style.opacity = "1";
+    } else {
+        content.style.opacity = "0.4";
+    }
+
+    // var person = {"FirstName":"Andrew","LastName":"Lock","Age":"31"};
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/Mouse?handler=FindUser",
+    //     // data: {id: id},
+    //     contentType: "application/json; charset=utf-8",
+    //     // dataType: "json",
+    //     // AntiforgeryToken is required by RazorPages
+    //     headers: {
+    //         RequestVerificationToken: $('input:hidden[name="__RequestVerificationToken"]').val()
+    //     },
+    //     data: JSON.stringify(person)
+    // }).done(function () {
+    //     alert("success");
+    // }).fail(function () {
+    //     alert("error");
+    // });
+
+    // $.ajax({
+    //     type: 'GET',
+    //     // Note the difference in url (this works if you're actually in Index page)
+    //     url: '/mouse?handler=FindUser',
+    //     headers: {
+    //         RequestVerificationToken: $('input:hidden[name="__RequestVerificationToken"]').val()
+    //     },
+    //     success: function (data) {
+    //         alert(data);
+    //     },
+    //     error: function (error) {
+    //         alert("Error: " + error);
+    //     }
+    // })
+}
+
+function handleDebugState(e) {
+    const DebugState = document.getElementById("DebugState");
+}
